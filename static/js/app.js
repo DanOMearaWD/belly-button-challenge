4e20aeaeff4bc38652378d9d28dcd475b619e368
Dan O'Meara <danomearawd@gmail.com>
2024-11-23 01:31:52 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,20 +3,26 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-    let metadata = data.metadata;
-    // Filter the metadata for the object with the desired sample number
-    let metadata_filtered = metadata.filter(each => parseInt(each.id) === parseInt(sample))[0]; //[0] to access first element in array
+    const metadata = data.metadata;
+
+    // Filter the metadata for the object with the desired sample number [0] to get first element in array
+    const metadata_filtered = metadata.filter(each => each.id == sample)[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-    let sample_metadata = d3.select('#sample-metadata');
+    const sample_metadata_element = d3.select('#sample-metadata');
 
     // Use `.html("") to clear any existing metadata
-    sample_metadata.html('');
+    sample_metadata_element.html('');
+
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-    Object.entries(metadata_filtered).forEach(([key, value]) => {
-      sample_metadata.append('p').text(key + ": " + value);
-    });
+    //Object.entries takes an oject and returns array of key value pairs. then
+    //foreach iterates and [key,value] deconstructs the 2 elements inside each array in foreach loop
+    Object.entries(metadata_filtered).forEach(
+      ([key, value]) => {
+        sample_metadata_element.append('p').text(`${key.toUpperCase()}: ${value}`);
+      }
+    );
   });
 }
 
@@ -25,14 +31,16 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-    let samples = data.samples;
-
+    const samples = data.samples;
     // Filter the samples for the object with the desired sample number
-    let samples_filtered = samples.filter(each => parseInt(each.id) === parseInt(sample))[0]; //[0] to access first element in array
+    const samples_filtered = samples.filter(each => parseInt(each.id) === parseInt(sample))[0]; //[0] to access first element in array
+
+
     // Get the otu_ids, otu_labels, and sample_values
-    let otu_ids = samples_filtered.otu_ids;
-    let otu_labels = samples_filtered.otu_labels;
-    let sample_values = samples_filtered.sample_values;
+    const otu_ids = samples_filtered.otu_ids;
+    const otu_labels = samples_filtered.otu_labels;
+    const sample_values = samples_filtered.sample_values;
+
     // Build a Bubble Chart
 
     //trace
@@ -62,14 +70,27 @@ function buildCharts(sample) {
     Plotly.newPlot('bubble', plot_data, layout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
-
+    const yticks = otu_ids.map(id => `OTU ${id}`);
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    let trace1_bar = {
+      x: sample_values.slice(0, 10).reverse(),
+      y: yticks.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      hoverinfo: "x+y+text",
+      type: "bar",
+      orientation: "h"
+    };
 
+    let data_bar = [trace1_bar];
+
+    let layout_bar = {
+      title: 'Top 10 Bacteria Cultures Found',
+      xaxis: { title: "Number of Bacteria" }
+    }
 
     // Render the Bar Chart
-
+    Plotly.newPlot("bar", data_bar, layout_bar);
   });
 }
 
@@ -78,42 +99,42 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-    let names = data.names;
-
+    const names = data.names;
     // Use d3 to select the dropdown with id of `#selDataset`
-    let selDataset_element = d3.select('#selDataset');
+    const select_element = d3.select('#selDataset');
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-    names.forEach(name => {
-      selDataset_element.append('option').text(name).property('value', name);
-    });
+    if (Array.isArray(names)) { //Make sure names is an array before looping through it
+      names.forEach(name => {
+        select_element.append('option').text(name).property('value', name);
+      });
+    }
 
     // Get the first sample from the list
-    let first_child_text = selDataset_element.property('value');
+    //dynamically generated elements cant be targeted with .select()
+    //use node() on d3 selection object to access native DOM node directly then use .value because first option is selected by default
+    const first_sample = select_element.node().value;
 
     // Build charts and metadata panel with the first sample
-
-    buildCharts(first_child_text);
-    buildMetadata(first_child_text);
-
-
-
-
+    buildMetadata(first_sample);
+    buildCharts(first_sample);
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-  if (parseInt(newSample) > 0) {  //this prevents error triggered by change event from initial population of dynamic option elements
+  //if positive number - prevents change event that is triggered in init() from returning window object as 'this' in event listener
+  if (parseInt(newSample) > 0) {
     buildMetadata(newSample);
+    buildCharts(newSample);
   }
 }
 
-//event listener for change in select element
-d3.select('#selDataset').on('change', optionChanged(this.value));
+//event listener
+d3.select('#selDataset').on('change', optionChanged(this));
 
 // Initialize the dashboard
 init();
